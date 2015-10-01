002b6c4e5aebcb6da37d083a38a4f36f78e95d61
Grabot <SanderKools@gmail.com>
2015-10-01 10:53:53 +0200

    Pit Modeling start

---
+++ 2IMV25_VR/Assets/movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	float speedX = 0;
+	float speedY = -1;
+	float speedZ = 0;
+	public float depth = -10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float pos = this.transform.position.y;
+		if (pos > depth) {
+			transform.Translate (new Vector3 (speedX, speedY, speedZ) * Time.deltaTime);
+		}
+	}
+}
